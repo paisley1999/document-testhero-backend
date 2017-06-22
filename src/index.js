eb973e578781f3bdecbaf0c041c293255cd36085
nopparat <nopparat.mueangkaew@gmail.com>
2017-06-22 21:37:15 +0700

    setup route success

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './containers/App';
+import { render } from 'react-dom';
+import {
+    BrowserRouter as Router,
+    Route
+} from 'react-router-dom';
+
 import registerServiceWorker from './registerServiceWorker';
 
 import './css/style.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './containers/App';
+import LoginForm from './components/LoginForm'
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+const router = (
+
+    <Router>
+        <div>
+            <Route exact path="/" component={App}/>
+            <Route path="/login" component={LoginForm}/>
+        </div>
+    </Router>
+
+);
+
+render(router, document.getElementById('root'));
 registerServiceWorker();
