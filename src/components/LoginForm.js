4ff026ea0836f54ed4a9993dbed89f786592c7a9
nopparat <nopparat.mueangkaew@gmail.com>
2017-06-24 00:03:28 +0700

    fix login layout

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -29,27 +29,32 @@ class LoginForm extends Component {
     }
 
     render() {
-        console.log('LoginForm Component')
         return (
-            <div className="">
-                <div className={`signup-form-control`}>
-                    <i className="material-icons">mail_outline</i>
-                    <div className="mdl-textfield mdl-js-textfield">
-                        <input id="txt_email" className="mdl-textfield__input" type="email" />
-                        <label className="mdl-textfield__label">Email</label>
-                    </div>
-                </div>
-                <div className={`signup-form-control`}>
-                    <i className="material-icons">lock_outline</i>
-                    <div className="mdl-textfield mdl-js-textfield">
-                        <input id="txt_password" className="mdl-textfield__input" type="password" />
-                        <label className="mdl-textfield__label">Password</label>
-                    </div>
-                </div>
-                <div className="signup-form-control signup-submit-button-box">
-                    <button onClick={this.signInWithEmailAndPassword} className="mdl-button mdl-js-button mdl-button--raised mdl-button--colored white-color">
-                        SIGN IN
-                    </button>
+            <div className="login-form">
+                <div className="mdl-layout mdl-js-layout mdl-color--grey-100">
+                    <main className="mdl-layout__content">
+                        <div className="mdl-card mdl-shadow--6dp">
+                            <div className="mdl-card__title mdl-color--primary mdl-color-text--white">
+                                <h2 className="mdl-card__title-text">9Picompany.</h2>
+                            </div>
+                            <div className="mdl-card__supporting-text">
+                                <div className="mdl-textfield mdl-js-textfield">
+                                    <input className="mdl-textfield__input" type="text" id="txt_email"/>
+                                    <label className="mdl-textfield__label" for="username">Username</label>
+                                </div>
+                                <div className="mdl-textfield mdl-js-textfield">
+                                    <input className="mdl-textfield__input" type="password" id="txt_password"/>
+                                    <label className="mdl-textfield__label" for="userpass">Password</label>
+                                </div>
+                            </div>
+                            <div className="mdl-card__actions mdl-card--border">
+                                <button onClick={this.signInWithEmailAndPassword}
+                                        className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+                                    SIGN IN
+                                </button>
+                            </div>
+                        </div>
+                    </main>
                 </div>
             </div>
         );
